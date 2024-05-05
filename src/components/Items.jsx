07d358dd742bd45171d70c9b1808268806998e08
Amitshringi <amitshringi0786@gmail.com>
2024-05-06 00:14:51 +0530

    Initial Commit

---
+++ src/components/Items.jsx
@@ -0,0 +1,23 @@
+
+import styles from "../styles/Item.module.css";
+
+import ItemCard from "./ItemCard";
+import itemData from "../data/itemData";
+import React from "react";
+
+function Items() {
+  return (
+    <div className={styles.wrapper}>
+      {itemData.map((item)=> (
+        <ItemCard 
+        key={item.id}
+        id={item.id}
+        name={item.name} price={item.price} />
+
+      ))}
+      
+    </div>
+  );
+}
+
+export default Items;
